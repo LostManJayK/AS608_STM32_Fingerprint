daf921d02e5aa5ea183c551bcec63faafb6b8d5b
Andrew <andrew@pop-os.lan>
2024-02-13 12:54:28 -0800

    Organizing file

---
+++ AS608.c
@@ -0,0 +1,15 @@
+#include "AS608.h"
+
+//Function for sending package to module
+void sendDataPackage(uint8_t*, uint8_t size)
+{
+    
+}
+
+//initializer for fingerprint module
+void fpInit()
+{
+    printf("Attempting connection with fingerprint modue...\n\r");
+
+
+}
